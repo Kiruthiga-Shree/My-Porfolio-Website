ccb4651b2e97a889e6512b5a11fe677ec00e84f1
Kiruthiga Shree <124908962+Kiruthiga-Shree@users.noreply.github.com>
2023-08-18 10:52:16 +0530

    Update Contact.jsx

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import girl from './assets/contact.png'
+import pdf from './assets/Resume.pdf'
 export const Contact = () => {
   return (
     <>
       <div className="contact" id='contact'>
         <h1>Contact</h1>
-        <div className="girl">
+        <div className="girl" id='section'>
           <img src={girl} alt="" />
         </div>
-        <div className="sources">
+        <div className="sources" id='section'>
           <div className="src" id='linkedin'>
                   <a href="https://www.linkedin.com/in/kiruthiga-shree-3a791220b" target='_blank' style={{textDecoration:"none",color:"#133F25"}}><h2><i class="fa-brands fa-linkedin"></i>  LINKEDIN</h2></a>
           </div>
@@ -17,6 +18,9 @@ export const Contact = () => {
           </div>
           <div className="src" id='mail'>
 <a href="mailto:kiruthigashree2002@gmail.com" target='_blank' style={{textDecoration:"none",color:"#133F25"}}><h2><i class="fa-solid fa-envelope"></i>  MAIL</h2></a>
+          </div>
+          <div className="src" id='pdf'>
+<a href={pdf} download="Kiruthiga-Shree-Resume.pdf" style={{textDecoration:"none",color:"#133F25"}}><h2><i class="fa-solid fa-file-lines"></i>  RESUME</h2></a>
           </div>
         </div>
       </div>
